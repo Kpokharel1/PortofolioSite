28e796fb5080b29ca345f6025ef973afdb16828d
Keshan1122 <krishnapokharel.2022@gmail.com>
2022-02-01 16:59:41 +0545

    home section created

--- js/script.js
+++ js/script.js
@@ -3,12 +3,24 @@ $(document).ready(function(){
         var scroll = $(window).scrollTop();
         
         
-        if(scroll>=80){
+        if(scroll>=100){
             $("sticky").addClass("stickyadd")
         }
         else{
             $(".sticky").removeClass("stickyadd")
         }
     })
+    
+   
+    
 
+})
+var typed = new Typed('.typed-words', {
+    strings: ["Scripts","Websites"," Mobile Apps"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 4000,
+    startDelay: 1000,
+    loop: true,
+    showCursor: true
 })
