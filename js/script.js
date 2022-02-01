02e06f6285d346f0bea6459dd1ecf6237100e23d
Keshan1122 <krishnapokharel.2022@gmail.com>
2022-02-01 18:00:19 +0545

    About section created

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
-$(document).ready(function(){
-    $(window).on('scroll',function(){
-        var scroll = $(window).scrollTop();
-        
-        
-        if(scroll>=100){
-            $("sticky").addClass("stickyadd")
-        }
-        else{
-            $(".sticky").removeClass("stickyadd")
-        }
-    })
-    
-   
-    
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
 
-})
-var typed = new Typed('.typed-words', {
-    strings: ["Scripts","Websites"," Mobile Apps"],
-    typeSpeed: 80,
-    backSpeed: 80,
-    backDelay: 4000,
-    startDelay: 1000,
-    loop: true,
-    showCursor: true
-})
+    if (scroll >= 100) {
+      $("sticky").addClass("stickyadd");
+    } else {
+      $(".sticky").removeClass("stickyadd");
+    }
+  });
+});
+var typed = new Typed(".typed-words", {
+  strings: ["Krishna Pokharel", "Web Developer", " Graphics Designer"],
+  typeSpeed: 80,
+  backSpeed: 80,
+  backDelay: 4000,
+  startDelay: 1000,
+  loop: true,
+  showCursor: true,
+});
